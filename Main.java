4f9808990efcbadea6a2a0c2c080ea24e7e80aa7
jose-menjivarhernandez <44983989+jose-menjivarhernandez@users.noreply.github.com>
2019-03-28 03:01:17 -0600

    Final Update Demo 3

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
 package userInterface;
-import controller.Driver;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 import logic.*;
 import controller.*;
+
+/**
+ * The class Main primarily serves as a starter and runner for the GUI and 
+ * uses the method displayMainMenu of an instance of Main Menu to run everything 
+ * within the GUI
+ * 
+ */
+
 public class Main extends Application{
 	MainMenu2 mainmenu = new MainMenu2();
-	GameOverAlertBox boxy = new GameOverAlertBox();
-	Driver newDriver = new Driver();
+	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
