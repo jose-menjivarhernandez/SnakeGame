54a5b85f4dd35e82f04f06dd3b5dab90403fe7bb
jose-menjivarhernandez <44983989+jose-menjivarhernandez@users.noreply.github.com>
2019-03-28 03:02:10 -0600

    Final Update Demo3

--- MainMenu2.java
+++ MainMenu2.java
@@ -25,22 +25,30 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+/**
+ * The class MainMenu2 (as a derivative of initial main menu in past demo) provides the user 
+ * with a menu graphical interface so as to choose the difficulty, and board desired. Immediately
+ * after these aspects are chosen, a specific scene is opened onto the stage, using
+ * the createAct method from the class Driver
+ * We used an instance of Driver "newDriver" in order to access the creation of a dynamic pane
+ * We instantiated our label "score" in order for the inner EventHandling class to update it.
+ * Our Stage will be what is ultimately depicted through the Graphical User Interface.
+ */
 
 public class MainMenu2  {
 	
-	Driver newDriver = new Driver();
-	
-	boolean easyClick = false;
-	boolean mediumClick = false;
-	boolean hardClick = false; 
-	boolean classicClicked = false;
-	boolean maze1Clicked = false;
-	boolean maze2Clicked = false;
+	static Driver newDriver = new Driver();
 	Label score = new Label();
 
 	public Stage stage1 = new Stage();
 	
-	
+	/**
+	 * 
+	 * The inner EventHandling class "ControlClass" handles the controls on keys pressed.
+	 * Immediately after event, Control Class switches directions with a switch statement
+	 * this gives order to our case switcher in the method createAct in newDriver
+	 *
+	 */
 	public class ControlClass implements EventHandler<KeyEvent>{
 
 		
@@ -72,6 +80,10 @@ public class MainMenu2  {
 
 		
 	}
+	/**
+	 * The DisplayMainMenu method displays all visuals in the GUI
+	 * @return the Stage with the scenes holding specifications for customized game plays 
+	 */
 
 	public Stage displayMainMenu() {
 		//Creating HBox and adding created labels to it
@@ -104,24 +116,19 @@ public class MainMenu2  {
 		//Setting first screen elements in window
 		labelling.setAlignment(Pos.CENTER);
 		buttonHolder.setAlignment(Pos.CENTER);
-		Button ReturnButton = new Button ("Return");
-		ReturnButton.setOnAction(e->{
-		stage1.close();
-		stage1 = displayMainMenu();
-		stage1.show();
-		});
 		
 		
-		//****Contents for Scene 1 are here
+		//Contents for Scene 1 are here
 		BorderPane mainMenu = new BorderPane();
 		mainMenu.setTop(labelling);
 		mainMenu.setCenter(buttonHolder);
 		Scene scene1 = new Scene(mainMenu, 900, 650);
 		stage1.setScene(scene1);
 		
-		//*Contents for Easy selection are here 
+		//Contents for Board Maze selection are here 
 		BorderPane selectionMenu = new BorderPane();		
 		VBox selectionHolder = new VBox(50);
+		
 		//Create 3 buttons for board options
 		Label selecting = new Label ("Please select your board");
 		selecting.setFont(Font.font("Verdana", 50));
@@ -131,6 +138,7 @@ public class MainMenu2  {
 		maze1.setPrefSize(200, 100);
 		Button maze2 = new Button ("Maze 2"); 
 		maze2.setPrefSize(200, 100);
+		
 		//Place all 3 buttons at the center of the window
 		selectionHolder.getChildren().addAll(selecting, classic, maze1, maze2);
 		selectionHolder.setAlignment(Pos.CENTER);
@@ -138,42 +146,60 @@ public class MainMenu2  {
 
 		Scene scene2 = new Scene(selectionMenu, 900, 650);
 		
+		//Add both buttons and timer to the scene
 		Timeline timeline = new Timeline();
 		BorderPane generalPane= new BorderPane();
+		
 		//Set "Return" and "Quit" buttons
+		//These buttons are immediately created with their specified 
+		
+		Button ReturnButton = new Button ("Return");
+		ReturnButton.setOnAction(e->{
+			stage1.close();
+			timeline.stop();
+			stage1 = displayMainMenu();
+			stage1.show();
+		});
+		
 		Button QuitButton = new Button("Quit");
 		ReturnButton.setPrefSize(75, 75);
 		QuitButton.setPrefSize(75, 75);
-		//Add both buttons and timer to the scene
 		
 		VBox sideButtons = new VBox(ReturnButton, QuitButton, score);
 		sideButtons.setAlignment(Pos.CENTER);
 		generalPane.setCenter(sideButtons);
 
-	    QuitButton.setOnAction(e->stage1.close());
+	    QuitButton.setOnAction(e->
+	    {stage1.close();
+	    timeline.stop();
+	    	}
+	    );
 		
-		
+		//These following lines of code dictate the specific actions of every specific 
+	   //button in the different menus shown stages in the GUI
 		easyButton.setOnAction(e->{
 			stage1.setScene(scene2);
-			easyClick = true;
 			newDriver.setDuration(0.1);
 		});
 
 		mediumButton.setOnAction(e->{
 			stage1.setScene(scene2);
-			mediumClick = true;
+		
 			newDriver.setDuration(0.075);
 		});
 
 		hardButton.setOnAction(e->{
 			stage1.setScene(scene2);
-			hardClick = true;
+			
 			newDriver.setDuration(0.05);
 			
 		});
 		
+		//The next few setOnAction statements are setting the buttons for pressing events
+		//that are immediately going to create a dynamic scene using the time-line, 
+		//duration, and type of Driver class to depict in the scene.
 		classic.setOnAction(e->{
-			classicClicked = true;
+			
 			newDriver.setType("classic");
 			generalPane.setLeft(newDriver.createAct(timeline, newDriver.getDuration(),newDriver.getType()));
 			Scene finalScene = new Scene(generalPane,900,650);
@@ -183,7 +209,7 @@ public class MainMenu2  {
 		});
 
 		maze1.setOnAction(e->{
-			maze1Clicked = true;
+			
 			newDriver.setType("m1");
 			generalPane.setLeft(newDriver.createAct(timeline, newDriver.getDuration(),newDriver.getType()));
 			Scene finalScene = new Scene(generalPane,900,650);
@@ -194,7 +220,7 @@ public class MainMenu2  {
 		});
 
 		maze2.setOnAction(e->{
-			maze2Clicked = true;
+			
 			newDriver.setType("m2");
 			generalPane.setLeft(newDriver.createAct(timeline, newDriver.getDuration(),newDriver.getType()));
 			Scene finalScene = new Scene(generalPane,900,650);
@@ -214,4 +240,5 @@ public class MainMenu2  {
 	
 	
 }
+
 
