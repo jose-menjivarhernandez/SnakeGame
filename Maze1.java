24dc23ebb211bd3d75335dff9f3230d455261512
kate-adams <47303073+kate-adams@users.noreply.github.com>
2019-04-11 20:43:56 -0600

    Update Maze1.java

--- Maze1.java
+++ Maze1.java
@@ -3,55 +3,50 @@ package logic;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-/**
- * The class Maze1 creates a series of rectangles that are later displayed onto a specific pane
- * This pane is later put into a dynamic KeyFrame and depicted in a scene in the GUI. 
- *
- */
 public class Maze1 {
 	int NODE_SIZE = 10;
 	int BOARD_WIDTH = 70 * NODE_SIZE;
 	int BOARD_HEIGHT = 65 * NODE_SIZE;
+	int BARRIER_WIDTH = 2;
 
 	public Rectangle addBarrier1() {
-		Rectangle rect1 = new Rectangle(NODE_SIZE * 40, NODE_SIZE * 25,NODE_SIZE * 30, NODE_SIZE * 2);
+		Rectangle rect1 = new Rectangle(NODE_SIZE * 40, NODE_SIZE * 25,NODE_SIZE * 30, NODE_SIZE * BARRIER_WIDTH);
 		rect1.setFill(Color.CHOCOLATE);
 		
 		return rect1;
 	}
 
 	public Rectangle addBarrier2() {
-		Rectangle rect2 = new Rectangle(NODE_SIZE * 40,NODE_SIZE * 45,NODE_SIZE * 2, NODE_SIZE * 20);
+		Rectangle rect2 = new Rectangle(NODE_SIZE * 40,NODE_SIZE * 45,NODE_SIZE * BARRIER_WIDTH, NODE_SIZE * 20);
 		rect2.setFill(Color.CHOCOLATE);
 		return rect2;
 	}
 
 	public Rectangle addBarrier3() {
-		Rectangle rect3 = new Rectangle(NODE_SIZE * 60,NODE_SIZE * 25,NODE_SIZE * 2, NODE_SIZE * 30);
+		Rectangle rect3 = new Rectangle(NODE_SIZE * 60,NODE_SIZE * 25,NODE_SIZE * BARRIER_WIDTH, NODE_SIZE * 30);
 		rect3.setFill(Color.CHOCOLATE);
 		
 		return rect3;
 	}
 
 	public Rectangle addBarrier4() {
-		Rectangle rect4 = new Rectangle(NODE_SIZE * 30,NODE_SIZE * 15,NODE_SIZE * 20, NODE_SIZE * 2);
+		Rectangle rect4 = new Rectangle(NODE_SIZE * 30,NODE_SIZE * 15,NODE_SIZE * 20, NODE_SIZE * BARRIER_WIDTH);
 		rect4.setFill(Color.CHOCOLATE);
 		
 		return rect4;
 	}
 		
 	public Rectangle addBarrier5() {
-		Rectangle rect5 = new Rectangle(NODE_SIZE * 20,NODE_SIZE * 35,NODE_SIZE * 40, NODE_SIZE * 2);
+		Rectangle rect5 = new Rectangle(NODE_SIZE * 20,NODE_SIZE * 35,NODE_SIZE * 40, NODE_SIZE * BARRIER_WIDTH);
 		rect5.setFill(Color.CHOCOLATE);
 		
 		return rect5;
 	}
 	
 	public Rectangle addBarrier6() {
-		Rectangle rect6 = new Rectangle(NODE_SIZE * 10,0,NODE_SIZE * 2, NODE_SIZE * 30);
+		Rectangle rect6 = new Rectangle(NODE_SIZE * 10,0,NODE_SIZE * BARRIER_WIDTH, NODE_SIZE * 30);
 		rect6.setFill(Color.CHOCOLATE);
 		
 		return rect6;
 	}
 }
-
