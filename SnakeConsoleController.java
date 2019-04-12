2eab28fc089d7c1e56e842be208a41057ad8345c
kate-adams <47303073+kate-adams@users.noreply.github.com>
2019-04-11 19:24:26 -0600

    Update SnakeConsoleController.java

--- SnakeConsoleController.java
+++ SnakeConsoleController.java
@@ -1,4 +1,11 @@
 package controller;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import logic.*;
 /**
@@ -19,29 +26,35 @@ public class SnakeConsoleController{
 	private char wall = '#';
 	private char back = '.';
 	public SnakeConsoleController(){}
-	
-	/**
-	 * This method is in charge of most things that happen in the main, including the board creation and 
-	 * the input reading and control
-	 */
+	String name;
+	String filename = "highscoreconsole.txt";
+	int counter = 0;
 	public void play() {
-		
+		//Getting name for high score
+		Scanner scanner = new Scanner(System.in);		
+		System.out.println("Enter your name: ");
+		name = scanner.nextLine();	
+
 		//Creates a specific board and an instance of Moving to use its methods
 		ClassicBoardConsole board = new ClassicBoardConsole (width, height, consoleBoard, snake, berry, wall, back);
 		Moving move = new Moving(board);
 		boolean go = true;		
-		int counter = 0;		
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Press 'a' to go left, 'w' to go up, 'd' to go right, and 's' to go down");
 	
+
+		//instructions
+		System.out.println("Press 'a' to go left, 'w' to go up, 'd' to go right, and 's' to go down");
+		
+		/**
+		 * This method is in charge of most things that happen in the main, including the board creation and 
+		 * the input reading and control
+		 */
+		//playing game
 		while (go) {
 		//Check if eating berry and calling for a re-spawn
 			if(!board.hasNotEaten()) {
 				board.initialPlaceBerry(board.getBerry());
 				counter++;
 			}
-	
 			
 			//Getting user input
 			char symbol = scanner.next().charAt(0);			
@@ -71,16 +84,42 @@ public class SnakeConsoleController{
 				 go = false;	
 				 System.out.println("GAME OVER\nSCORE: " +counter);
 			}			
-			
-			//Checking if you died
+			//ending game
 			if(board.gameOver()) {
 				System.out.println("LOST");
-				go = false;
-			}		 			  
+			
+			//Creating file
+				File file = new File (filename);
+				if(file.exists()) {
+					
+				}
+				go = false;	 
+				//"Saving" score to file, this also overwrites the old file so it is empty
+				try {
+					FileWriter fwriter = new FileWriter(filename, true);
+					BufferedWriter buffwriter = new BufferedWriter (fwriter);
+					buffwriter.write(name +":" +Integer.toString(counter));
+					buffwriter.newLine();
+					buffwriter.close();
+				}catch(FileNotFoundException fnfe) {}
+				catch (IOException ioe) {}
+				//Reading and reporting score from file
+				try {
+				FileReader freader = new FileReader(filename);
+				BufferedReader reader = new BufferedReader(freader);
+				while(reader.readLine()!=null ||reader.readLine() == "\n") {
+					System.out.println(reader.readLine()); 
+				}
+				reader.close();
+				}catch (FileNotFoundException fnfe) {}
+				catch (IOException ioe) {}
+			}
+		
 		}
 		//Closing the scanner to ensure protection for privacy leaks
 		scanner.close();
 	}
+	
 	//***MAIN***
 	public static void main(String[] args) {
 		SnakeConsoleController snake = new SnakeConsoleController();
